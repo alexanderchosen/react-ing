97879dc1a0608ebe69bb765e473054753b67fe44
alexander chosen okon <chosenokonalexander@gmail.com>
2024-03-15 04:40:37 +0100

    my first react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App (){
+  return (
+    <div>
+      <h3>My first React Experience </h3>
+      <p> I am loving this new react experience. Hey! It's all about components, right?</p>
+      <p> I can't wait to see the world-class applications i will build using react. Let's keep react-ing!</p>
+    </div>
+  )
+}
+
+export default App;
